3fcfd6e365763de0da52d4a056b03089c559e1eb
Steven Spagnolo <steven@ssquared.co.uk>
2018-04-10 02:39:55 +0100

    Wuzzy was a bear

---
+++ wuzzy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  wuzzy
+//
+//  Created by Steven Spagnolo on 10/4/18.
+//  Copyright © 2018 Steven Spagnolo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
